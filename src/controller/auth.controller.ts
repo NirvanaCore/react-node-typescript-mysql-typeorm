ebeecdf59e3bb6b8201341cc882bb657e4a98e37
Soniya <soniya>
2022-05-22 13:03:54 +0530

    register and login

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,7 +1,10 @@
 import {Request, Response} from "express";
 import {RegisterValidation} from "../validation/registration.validation";
+import {getManager} from "typeorm";
+import bcrypt from 'bcryptjs'
+import {User} from "../entity/user.entity";
 
-export const Register = (req:Request,res:Response)=>{
+export const Register = async(req:Request,res:Response)=>{
     const body = req.body;
     const { error} = RegisterValidation.validate(body);
     if(error) return res.status(400).send(error.details);
@@ -10,5 +13,35 @@ export const Register = (req:Request,res:Response)=>{
         return res.status(400).send({message: "Password's dnt match"})
     }
 
-    res.send(body)
+    const repository = getManager().getRepository(User);
+
+    const {password, ...user} = await repository.save({
+        first_name:body.first_name,
+        last_name:body.last_name,
+        email:body.email,
+        password:await bcrypt.hash(body.password,10),
+    });
+
+    res.send(user)
+}
+
+export const Login = async(req:Request,res:Response)=>{
+    // const {email,password} = req.body
+    const repository = getManager().getRepository(User)
+    const user = await repository.findOneBy({email: await req.body.email});
+    if(!user){
+        return res.status(404).send({
+            message: 'user not found'
+        })
+    }
+
+    if(!await bcrypt.compare(req.body.password, user.password) ){
+        return res.status(404).send({
+            message: 'invalid credentials'
+        })
+    }
+
+    const {password , ...data} = user;
+
+    res.send(data);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
 import express from 'express';
 import cors from 'cors';
 import {routes} from "./routes";
+import {createConnection} from "typeorm";
+
+
 const PORT  = 8000;
 
-const app = express();
+  createConnection().then((connection)=>
+    {
+        const app = express();
 
-app.use(express.json());
-app.use(cors({
-    origin:["http://loaclhost:3000"]
-}))
+        app.use(express.json());
+        app.use(cors({
+            origin: ["http://loaclhost:3000"]
+        }))
 
-routes(app);
+        routes(app);
 
-app.listen(PORT,()=>{
-    console.log(`Server is Listening at port ${PORT}`)
-})
+        app.listen(PORT, () => {
+            console.log(`Server is Listening at port ${PORT}`)
+        })
+    }).catch(err=>console.error({err}))
+
